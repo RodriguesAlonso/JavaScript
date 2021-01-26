61bd23d2fcddbad72f9949bca334224da0c058c6
João Alonso <joaoalonso42@gmail.com>
2021-01-26 17:14:15 -0300

    study js

--- modulo03/ex010.js
+++ modulo03/ex010.js
@@ -1,4 +1,4 @@
-var idade = 18
+/*var idade = 18
     if (idade >= 18){
         console.log('voto obrigatório')
     }else if(idade >=16 ){
@@ -6,3 +6,49 @@ var idade = 18
     }else{
         console.log('não pode votar')
     }
+    
+   var agora = new Date()
+   console.log(agora)
+   var hora = agora.getHours()
+   console.log(hora)
+   */
+
+   var agora = new Date()
+   var diasem = agora.getDay()
+   
+   switch(diasem){
+       case 0:
+           console.log('domingo')
+           break
+       
+       case 1:
+           console.log('segunda-feira')
+           break
+
+       
+       case 2:
+           console.log('terça-feira')
+           break
+
+       
+       case 3:
+           console.log('quarta-feira')
+           break
+
+       
+       case 4:
+           console.log('quinta-feira')
+           break
+           
+       
+       case 5:
+           console.log('sexta-feira')
+           break
+
+       
+       case 6:
+           console.log('sabado')
+           break
+
+       
+   }
