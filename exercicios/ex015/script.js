9127cca3bd2060f17e96bd64fb876656e650b1a7
João Alonso <joaoalonso42@gmail.com>
2021-01-27 14:30:22 -0300

    js ex015

--- exercicios/ex015/script.js
+++ exercicios/ex015/script.js
@@ -1,3 +1,9 @@
-
+function verificar(){
+    var nascimento = window.document.getElementById(ano)
+    var mas = window.document.getElementById(masculino)
+    var femi = window.document.getElementById(feminino)
+    var verifica = window.document.getElementById(ver)
+    
+}
 
 
