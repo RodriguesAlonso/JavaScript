10459836be71370ffe2a4c2bb46c9797e7e97781
João Alonso <joaoalonso42@gmail.com>
2021-01-27 10:07:27 -0300

    js bom dia

--- exercicios/ex014/script.js
+++ exercicios/ex014/script.js
@@ -1,20 +1,23 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-    var data = new Date()    
+    var data = new Date()        
     var hora = data.getHours()    
     //msg.innerHTML = `Agora são ${hora} horas`     
     if(hora >= 5 && hora < 12){
-        msg.innerHTML = 'Bom dia'
+        msg.innerHTML = `Agora são ${hora} horas`
         img.src = 'img/manha_250.png'
+        window.document.body.style.backgroundColor = '#eee1d1' 
     }
     else if (hora >= 12 && hora <= 18){
-        msg.innerHTML = 'Boa tarde'
+        msg.innerHTML = `Agora são ${hora} horas`
         img.src = 'img/tarde_250.png'
+        window.document.body.style.backgroundColor = '#cc795e'
 
     }
     else{        
-        msg.innerHTML = 'Boa noite'
+        msg.innerHTML = `Agora são ${hora} horas`
         img.src = 'img/noite_250.png'
+        window.document.body.style.backgroundColor = '#508dba'
     }
 }
