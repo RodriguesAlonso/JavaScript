7abb135cb2a2edf77bef81dec2d706a217661e5f
João Alonso <joaoalonso42@gmail.com>
2021-01-31 13:08:28 -0300

    treino 2 finished

--- treinos/treino02/script.js
+++ treinos/treino02/script.js
@@ -1,8 +1,45 @@
 function verificar(){
+
     var data = new Date()
     var agora = data.getFullYear()
-    var anos = Number(ano.value)
-    var idade = agora - anos
-    var p1 = document.getElementsByTagName('p')[0]
-    p1.innerHTML = (`Sua idade é de ${idade} anos`)
+    var anos = Number(ano.value)        
+    var res = document.querySelector('div#res')        
+         
+    if (anos == 0 || anos > 2021){
+        window.alert('preencha o campo Ano de nascimento com um ano válida')   
+         
+    } else{
+        var idade = agora - anos
+        var fsex = document.getElementsByName('sexo')        
+        var genero = ''
+        var img = document.createElement('img')        
+        img.setAttribute('id', 'foto')
+    }if(fsex[0].checked){
+        genero = 'Homem'        
+        if( idade <  18){            
+            img.setAttribute('src', 'img/menino.jpg')
+        }else{
+            img.setAttribute('src', 'img/adulto.jpg')
+        }             
+            
+        
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+        if(idade < 18){
+            img.setAttribute('src', 'img/menina.jpg')
+        }else{
+            img.setAttribute('src', 'img/adulta.jpg')
+        }
+
+        
+        }       
+        
+        
+    
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`        
+        res.style.textAlign = 'center'
+        res.appendChild(img)    
 }
+
+
+
